e872ba6a9b458cc56acfcb2cbe9458ca2cebc2ca
majdeddine <majda-jambouri@hotmail.fr>
2017-12-23 20:03:26 +0000

    add quote and the author to the html page

--- js/index.js
+++ js/index.js
@@ -1,14 +1,21 @@
 $(document).ready(function(){
-  $.ajax({
-    headers: {
-      "X-Mashape-Key": config.KEY,
-      Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded"
-    },
-    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
-    success: function(data) {
-      console.log(data)
-      }
+  function get_quote(){
+    $.ajax({
+      headers: {
+        "X-Mashape-Key": config.KEY,
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+      success: function(d) {
+          var data = JSON.parse(d)
+          $('.quote').html(data.quote)
+          $('.author').html(data.author)
+        }
 
-    })
+      })
+
+  }
+
+  get_quote()
 })
