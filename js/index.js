55f35408ba76983d9ad5b3295d8a80e67ecf6f91
majdeddine <majda-jambouri@hotmail.fr>
2017-12-23 22:48:09 +0000

    add twitter share button

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  $(".twitter-share-button").attr('data-text', `{data.quote}`)
   function get_quote(){
     $.ajax({
       headers: {
@@ -9,17 +10,20 @@ $(document).ready(function(){
       url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
       success: function(d) {
           var data = JSON.parse(d)
+          $(".twitter-share-button").attr('href', `https://twitter.com/intent/tweet?text=${data.quote}`)
+          $(".twitter-share-button").attr('data-hashtags', data.author)
+          // $(".twitter-share-button").attr('href', `https://twitter.com/share?text=${data.quote}`)
           $('.quote').html(data.quote)
           $('.author').html(data.author)
+          console.log($(".twitter-share-button").attr('data-text'))
         }
-
       })
-
   }
 
   get_quote()
-  
-  $('#s').on('click',function(){
+
+  $('.new-quote-btn').on('click',function(){
     get_quote()
   })
+
 })
