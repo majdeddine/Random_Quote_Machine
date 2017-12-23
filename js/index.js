e8811e80c0a7bcc2f9daac533f6b003c135c6adb
majdeddine <majda-jambouri@hotmail.fr>
2017-12-23 19:31:24 +0000

    add eslint

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-
+$(document).ready(function(){
+  $.getJSON("https://quotesondesign.com/wp-json/posts?", function(a) {
+  $("body").append(a[0].content + "<p>&mdash; " + a[0].title + "</p>")
+});
+})
